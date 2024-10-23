426c5d93c32da0b41f2613ca065474682bf65318
uglyduckling30 <120159510+uglyduckling30@users.noreply.github.com>
2024-10-23 12:09:30 -0600

    Update CustomListTest.java

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -12,5 +12,36 @@ import java.util.ArrayList;
 
 public class CustomListTest {
 
+  public int getCount(){
+    return cities.size();
+}
+  public void addCity(City city){
+}
+
+  private CustomList list;
+/**
+* create a mocklist for my citylist
+* @return
+*/
+public CustomList MockCityList(){
+list = new CustomList(null,new ArrayList<>());
+return list;
+}
+
+  /**
+* get the size of the list
+* increase the list by adding a new city
+* check if our current size matches the initial size
+plus one
+*/
+@Test
+public void addCityT est(){
+list = MockCityList();
+int listSize = list.getCount();
+list.addCity(new City("Estevan"
+,
+"SK"));
+assertEquals(list.getCount(),listSize + 1);
+}
 
 }
